c48f7e578287e19c4340aded66c48be910aa7026
Spencer Belton <23slowmojo@gmail.com>
2021-06-24 08:33:52 -0600

    Buttons added to custom reusable view

---
+++ ShoeShockApp/View/SectionHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  SectionHeaderView.swift
+//  ShoeShockApp
+//
+//  Created by Spencer Belton on 6/24/21.
+//
+
+import UIKit
+
+class SectionHeaderView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
